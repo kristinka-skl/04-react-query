c9b6bb5409eab6d6770f0395d72d1a086506f5c9
kristinka-skl <kristinka.skl@gmail.com>
2025-10-05 20:07:39 +0200

    fixed placeholder and toast

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import css from "./App.module.css";
 import "modern-normalize";
 import SearchBar from "../SearchBar/SearchBar";
@@ -9,26 +9,29 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import ReactPaginate from "react-paginate";
 
 export default function App() {
   const [topic, setTopic] = useState<string>("");
   const [currentPage, setPage] = useState<number>(1);
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["topic", topic, currentPage],
     queryFn: () => fetchMovies(topic, currentPage),
     enabled: topic !== "",
+    placeholderData: keepPreviousData,
   });
   const totalPages = data?.total_pages ?? 0;
   const [modalData, setModalData] = useState<Movie | null>(null);
   const handleSearch = async (topic: string) => {
     setTopic(topic);
     setPage(1);
-    if (data?.results && data.results.length === 0) {
+  };
+  useEffect(() => {
+    if (isSuccess && data?.results.length === 0) {
       toast("No movies found for your request");
     }
-  };
+  }, [isSuccess, data]);
   const handleSelect = (data: Movie) => {
     setModalData(data);
   };
